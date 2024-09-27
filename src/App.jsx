ca455b20c50faeb4205b844372a0635dd5b66e3a
ERNEST CHRIS <ernestchris6@gmail.com>
2024-09-27 23:38:30 +0100

    other pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
 import Home from './Pages/Home';
 import Doctors from './Pages/Doctors'
 import About from './Pages/About';
 import Contact from './Pages/Contact';
 import Appointment from './Pages/Appointment';
-import Navbar from './components/Navbar';
-
+import Create from './Pages/Create';
+import Login from './Pages/Login';
+import Error from './Pages/Error';
 
 
 export default function App() {
@@ -17,14 +19,13 @@ export default function App() {
        <Routes>
           <Route index element={<Home />}/>
           <Route path='doctors' element={<Doctors />} />
+          <Route path='doctors/:appointment' element={<Appointment />} />
           <Route path='about' element={<About />} />
           <Route path='contact' element={<Contact />} />
-          <Route path='create' element={<Doctors />} />
-          <Route path='doctors' element={<Doctors />} />
-          <Route path='doctors' element={<Doctors />} />
-          <Route path='doctors' element={<Doctors />} />
-          <Route path='appointment' element={<Appointment />} />
-       </Routes>
+          <Route path='create' element={<Create />} />
+          <Route path='login' element={<Login />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
     </div>
   )
 }
