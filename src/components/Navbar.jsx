01ea1dac5906f28941cee2c8a19b5798bc51fd6e
ERNEST CHRIS <ernestchris6@gmail.com>
2024-09-30 00:21:34 +0100

    colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,28 +9,28 @@ export default function Navbar() {
 
   const [isLogin, setIsLogin] = useState(false)
   return (
-    <div className='flex items-center justify-around mt-4'>
+    <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
        {/* <img src={assets.doc1} alt='logo' /> */}
-       <img className='w-16 h-16' src={logo} alt='logo'/>
-       <ul className='flex items-center justify-around gap-6'>
+       <img className='w-44 cursor-pointer' src={logo} alt='logo'/>
+       <ul className='hidden md:flex items-center justify-around gap-6'>
         <NavLink>
-            <li>HOME</li>
-            <hr />
+            <li className='py-1'>HOME</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
         </NavLink>
 
         <NavLink>
-            <li>ALL DOCTORS</li>
-            <hr />
+            <li className='py-1'>ALL DOCTORS</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
         </NavLink>
 
         <NavLink>
-            <li>ABOUT</li>
-            <hr />
+            <li className='py-1'>ABOUT</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
         </NavLink>
 
         <NavLink>
-            <li>CONTACT</li>
-            <hr />
+            <li className='py-1'>CONTACT</li>
+            <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto'/>
         </NavLink>
 
        </ul>
