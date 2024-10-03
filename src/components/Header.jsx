c66b266322eb20c0b6d7cda6fe819700ea1065f8
ERNEST CHRIS <ernestchris6@gmail.com>
2024-10-03 23:55:41 +0100

    header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,20 +6,21 @@ import headimg from '../assets/header_img.png'
 
 export default function Header() {
   return (
-    <div  className='flex'>
+    <div  className='flex flex-col md:flex-row flex-wrap bg-primary rounded-lg px-6 md:px-10 lg:px-20'>
+
       {/*---------Left side ----------- */}
-      <div>
-        <p>Book Appointment  <br /> With Trusted Doctors</p>
-        <div>
-          <img src={head} alt="" />   
-          <p>Simply browse through our extensive list of trusted doctors<br /> Schedule an appointment today...</p>   
+      <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] md:mb-[-30px]'>
+        <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>Book Appointment  <br /> With Trusted Doctors</p>
+        <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
+          <img className='w-28' src={head} alt="" />   
+          <p>Simply browse through our extensive list of trusted doctors<br className='hidden md:block'/> Schedule an appointment today...</p>   
         </div>
-        <a href="">Book Appointment <img src={arrow} alt="" /></a>
+        <a className='flex items-center gap-2 bg-white rounded-full px-8 py-3 text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300' href="#speciality">Book Appointment <img className='w-3' src={arrow} alt="" /></a>
       </div>
       
       {/*---------Right side ----------- */}
-      <div>
-        <img src={headimg} alt="" />
+      <div className='md:w-1/2 relative'>
+        <img className='w-full md:absolute bottom-0 h-auto rounded-lg' src={headimg} alt="" />
       </div>
     </div>
   )
