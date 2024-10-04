80631540bcdf11aeafee7356b63349a77d62f540
ERNEST CHRIS <ernestchris6@gmail.com>
2024-10-04 23:49:57 +0100

    removing errs

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -11,7 +11,7 @@ import tick_icon from './tick_icon.svg'
 import appointments_icon from './appointments_icon.svg'
 import earning_icon from './earning_icon.svg'
 import patients_icon from './patients_icon.svg'
-import logo_icon from './logo.svg';
+import logo_icon from './logo.svg'
 import doc1 from './doc1.png'
 
 
--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-// import { specialityData } from '../assets/assets';
-// import { Link } from 'react-router-dom';
+
+
+
 
 export default function SpecialityMenu() {
   return (
@@ -10,12 +11,7 @@ export default function SpecialityMenu() {
         </h1>
         <p>Simply browse through our list of trusted doctors, schedule your appointment hassle free</p>
         <div>
-            {specialityData.map((item, index)=> (
-                <Link to={`/doctors/${item.speciality}`}>
-                    <img src={item.image} alt="" /> 
-                    <p>{item.speciality}</p>
-                </Link>
-            ))}
+        
         </div>
     </div>
   )
