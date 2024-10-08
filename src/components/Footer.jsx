d9eb7e1c9f29adcd8c6e5327ce35014eda3c85a6
ERNEST CHRIS <ernestchris6@gmail.com>
2024-10-08 12:40:50 +0100

    key

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='bg-primary p-2 w-full'>Footer</div>
+    <div className='bg-primary p-2 w-full mb-8'>Footer</div>
   )
 }
