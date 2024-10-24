71c0e904e8c1341e7807af2a126b9ce6fa011769
ERNEST CHRIS <ernestchris6@gmail.com>
2024-10-24 09:31:35 +0100

    sign up page"

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Login() {
+
+  const [state, setState] = useState("Sign Up")
+
+  function handleSubmit(e){
+      e.preventDefault();
+  }
+
+
   return (
-    <div>Login</div>
+    <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center m-auto p-8'>
+        <div className=''>
+          <p>{state === "Sign Up" ? "Sign Up" : "Login"}</p>
+        </div>
+
+        {state === "Sign Up" && <div>
+            <p>Name:</p>
+            <input type="text" placeholder='Enter name' />
+        </div>}
+
+        <div>
+            <p>Email:</p>
+            <input type="email" placeholder='Enter name' />
+        </div>
+
+        <div>
+            <p>Password:</p>
+            <input type="password" placeholder='Enter name' />
+        </div>
+        
+        <button className='bg-primary w-full text-white'>{state === "Sign Up" ? "Sign Up" : "Login"}</button>
+
+        <p>{state === "Sign Up" ? "Already have an account": "Don't have an account" } <span className='underline text-primary'>{state === "Sign Up" ? "Login" : "Sign Up"}</span></p>
+    </form>
   )
 }
