1a5c5ea7bfde9f6f60cd4b3d685f69eff15d52ad
ERNEST CHRIS <ernestchris6@gmail.com>
2024-10-25 09:15:23 +0100

    final touches

--- src/Pages/Doctors.jsx
+++ src/Pages/Doctors.jsx
@@ -30,7 +30,7 @@ export default function Doctors() {
     <div>
       <p className='text-gray-600'>Browse through the doctors specialist.</p>
       <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
-        <button className={`py-1 px-3 border rounded text-sm transition-all duration-500 sm:hidden ${showFilter ? 'bg-primary' : ""}`} onClick={()=> setShowFilter(prev => !prev)}>Filters</button>
+        <button className={`py-1 px-3 border rounded text-sm transition-all duration-500 sm:hidden ${showFilter ? 'bg-primary' : ""} ${showFilter ? 'text-white' : 'text-black'}`} onClick={()=> setShowFilter(prev => !prev)}>Filters</button>
         <div className={`flex-col gap-4 text-sm text-gray-600 ${showFilter ? 'flex' : 'hidden sm:flex'}`}>
           <p onClick={()=> speciality === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')} 
           className={`w-[90vw] hover:bg-indigo-100 sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'General physician' ? "bg-indigo-100 text-black" : ""}`}>General physician</p>
