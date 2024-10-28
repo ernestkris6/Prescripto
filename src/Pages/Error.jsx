58a904c784ae848d5d1895d76f348ec8657e2c33
ERNEST CHRIS <ernestchris6@gmail.com>
2024-10-28 23:44:26 +0100

    redirect messages

--- src/Pages/Error.jsx
+++ src/Pages/Error.jsx
@@ -4,7 +4,7 @@ export default function Error() {
   return (
     <div className='bg-black/80 m-auto justify-center items-center rounded py-8 max-w-96 shadow-2xl mt-24 hover:bg-primary transition-all hover:translate-x-[10px] duration-500'>
      <p className='text-center font-bold m-auto justify-center py-16 text-2xl text-white/80 hover:text-black/80'>Error: page not found !!!</p>
-     <p></p>
+     <p>Click here to go back to the home page</p>
     </div>
   )
 }
