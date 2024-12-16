a3cf652617ee04f53233bb4ba0a9f42e3b6b0414
ERNEST CHRIS <ernestchris6@gmail.com>
2024-12-16 22:48:40 +0100

    authContex

---
+++ src/Context/AuthContext.js
@@ -0,0 +1,14 @@
+import { createContext } from "react";
+
+
+const AuthContext = createContext()
+
+const initialState = {
+    user : null,
+    isAuthenticated : false,
+}
+
+function AuthProvider(){
+    
+    
+}
