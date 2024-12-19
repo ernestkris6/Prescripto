4f4f863a69c911f1af4663a52ecb600d880d660d
ERNEST CHRIS <ernestchris6@gmail.com>
2024-12-19 23:21:21 +0100

    rewriting codes

--- src/Context/AuthContext.js
+++ src/Context/AuthContext.js
@@ -1,72 +1,179 @@
-import { createContext, useContext, useReducer } from "react";
+const { useReducer } = require("react");
+const { createContext } = require("react");
 
 
-const AuthContext = createContext()
+const appointmentContext = createContext();
 
 const initialState = {
     user : null,
     isAuthenticated : false,
 }
 
-
 function reducer(state, action){
     switch(action.type){
-        case "login": 
-        return{
+        case 'login': return {
             ...state,
-            user: action.payload,
+            user: action.paylod,
             isAuthenticated: true,
-            
         }
-        case "logout": 
-        return{
+        case 'logout': return {
             ...state,
             user: null,
             isAuthenticated: false,
         }
 
-        default : throw new Error("Incorrect details")
+        default: throw new Error('Unauthorized route...')
     }
 }
 
-const userDetails = {
-    password : "qwerty",
-    name : "Kris",
-}
 
-function AuthContextProvider({children}){
-    
-    const [{user, isAuthenticated}, dispatch] = useReducer(reducer, initialState);
-   
-    function login(name,password){
-        if(name === userDetails.name && password === userDetails.password){
-            dispatch({type: "login", payload: userDetails})
-        }
-    }
+function AppointmentContext({children}){
 
-    function logout(){
-        dispatch({type: "logout"})
-    }
+    const [state, dispatch] = useReducer();
+
 
     const value = {
-        user,
-        isAuthenticated,
-        login,
-        logout,
-        userDetails,
-        dispatch,
+
     }
 
-    return <AuthContext.Provider value={value}> 
+    return <AppointmentContext.Provider value={value}>
         {children}
-    </AuthContext.Provider>
+    </AppointmentContext.Provider>
+
+    
 }
 
-function useAuth(){
-   const context = useContext();
-   if(context === undefined) throw new Error("AuthContext was used in the wrong component...")
 
-    return context;
-}
 
-export {AuthContextProvider, useAuth}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { createContext, useContext, useReducer } from "react";
+
+
+// const AuthContext = createContext()
+
+// const initialState = {
+//     user : null,
+//     isAuthenticated : false,
+// }
+
+
+// function reducer(state, action){
+//     switch(action.type){
+//         case "login": 
+//         return{
+//             ...state,
+//             user: action.payload,
+//             isAuthenticated: true,
+            
+//         }
+//         case "logout": 
+//         return{
+//             ...state,
+//             user: null,
+//             isAuthenticated: false,
+//         }
+
+//         default : throw new Error("Incorrect details")
+//     }
+// }
+
+// const userDetails = {
+//     password : "qwerty",
+//     name : "Kris",
+// }
+
+// function AuthContextProvider({children}){
+    
+//     const [{user, isAuthenticated}, dispatch] = useReducer(reducer, initialState);
+   
+//     function login(name,password){
+//         if(name === userDetails.name && password === userDetails.password){
+//             dispatch({type: "login", payload: userDetails})
+//         }
+//     }
+
+//     function logout(){
+//         dispatch({type: "logout"})
+//     }
+
+//     const value = {
+//         user,
+//         isAuthenticated,
+//         login,
+//         logout,
+//         userDetails,
+//         dispatch,
+//     }
+
+//     return <AuthContext.Provider value={value}> 
+//         {children}
+//     </AuthContext.Provider>
+// }
+
+// function useAuth(){
+//    const context = useContext();
+//    if(context === undefined) throw new Error("AuthContext was used in the wrong component...")
+
+//     return context;
+// }
+
+// export {AuthContextProvider, useAuth}
